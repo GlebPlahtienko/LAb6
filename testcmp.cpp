2bb95cfa898b1ded048ce8179b4e5cd0fad8b92a
Plahtienko <ke1717pgl31@susu.ru>
2021-05-28 23:42:13 +0500

    исправлено форматирование файла testcmp.cpp

--- testcmp.cpp
+++ testcmp.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include     "mycomplex.h"
+
 using namespace std;
+
 int main(){//тестирование класса комплексных чисел
+
  Complex A,B(-4), C(23.0, 45.9);
 cout<<A <<","<< B<<","<<C <<endl; A=B+C;
 cout<<"A=B+C,A="<< A<<endl;
