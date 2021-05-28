5e225b234671e038383828c974f711d7e6f775bf
Plahtienko <ke1717pgl31@susu.ru>
2021-05-28 23:52:54 +0500

    исправлено форматирование файла testcmp.cpp

--- testcmp.cpp
+++ testcmp.cpp
@@ -2,14 +2,9 @@
 #include "mycomplex.h"
 using namespace std;
 
-/** @brief Main function for Testing complex numbers
-    *
-    */
+
 int main() {
-    /** @param A - Complex value with Real = 0 and Imagine = 0
-     *  @param B - Complex value with Real = -4 and Imagine = 0
-     *  @param C - Complex value with Real = 23.0 and Imagine = 0
-     */
+
   Complex A, B(-4), C(23.0, 45.9);
   cout << A << "," << B << "," << C << endl;
   A = B + C;
@@ -20,7 +15,7 @@ int main() {
   cout << "M*A=" << M * A << endl;              // Output for multiply of M and A
   cout << "M/4.45=" << M / 4.45 << endl;        ///<Output for division of M by 4.45
 
-    ///@param D - Complex value entered by user
+
   Complex D;
   cout << "Enter complex number D = ";
   cin >> D;                                     ///<Waiting for entering D
